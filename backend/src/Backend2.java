4fad163577192a9faf0591cfab1a11ca72a383ce
Rui Ji <ruiji@usc.edu>
2022-11-21 18:17:08 -0800

    ray version 3

--- backend/src/Backend2.java
+++ backend/src/Backend2.java
@@ -1,19 +1,19 @@
-import RateMyClass.Server;
-import RateMyClass.CourseMap;
+import java.util.Map;
 
 public class Backend2 {
+
     public boolean CourseExist(Course c){
         CourseMap temp = Server.getAllCourses();
-        Map<String, User> temp_map = CourseMap.map;
+        Map<String, Course> temp_map = temp.map;
         if(temp_map.containsValue(c)) return true;
         return false;
     }
 
     public boolean addCourse(String id, String name, String description){
-        Course c = new Course(String id, String name, String description);
+        Course c = new Course(id, name, description);
         if(CourseExist(c)) return false;
         else {
-            Server.postCourse(course);
+            Server.postCourse(c);
             return true;
         }
     }
--- backend/src/Server.java
+++ backend/src/Server.java
@@ -9,13 +9,13 @@ import com.google.gson.Gson;
 public class Server {
 	
 	public static void main(String[] args) {
-		// Course c = new Course("CSCI 270", "Algorithms", "Greedy things lol");
-		// c.addRating(new Rating(5, "", "Kempe", "Fall 2022", "AMAZING!"));
-		// putCourse("-NHRUlzjbCY-o7vaXx4y", c);
-		// CourseMap courses = getAllCourses();
-        // for(String key : courses.map.keySet()) {
-        //     System.out.println(courses.map.get(key));
-        // }
+//		 Course c = new Course("CSCI 270", "Algorithms", "Greedy things lol");
+//		 c.addRating(new Rating(5, "", "Kempe", "Fall 2022", "AMAZING!"));
+//		 putCourse("-NHRUlzjbCY-o7vaXx4y", c);
+//		 CourseMap courses = getAllCourses();
+//         for(String key : courses.map.keySet()) {
+//             System.out.println(courses.map.get(key));
+//         }
 	}
 
 	public static void putCourse(String courseIdentifier, Course course) {
