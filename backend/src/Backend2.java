0084c1e277876fd7698cde2ab528c740368d5c8e
irith-k <imkatiya@usc.edu>
2022-11-21 17:58:03 -0800

    errors fixed

--- backend/src/Backend2.java
+++ backend/src/Backend2.java
@@ -1,19 +1,15 @@
-import RateMyClass.Server;
-import RateMyClass.CourseMap;
-
 public class Backend2 {
     public boolean CourseExist(Course c){
         CourseMap temp = Server.getAllCourses();
-        Map<String, User> temp_map = CourseMap.map;
-        if(temp_map.containsValue(c)) return true;
+        if(temp.map.containsValue(c)) return true;
         return false;
     }
 
     public boolean addCourse(String id, String name, String description){
-        Course c = new Course(String id, String name, String description);
+        Course c = new Course(id, name, description);
         if(CourseExist(c)) return false;
         else {
-            Server.postCourse(course);
+            Server.postCourse(c);
             return true;
         }
     }
